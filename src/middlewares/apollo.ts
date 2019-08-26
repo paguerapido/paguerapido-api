8c143ddda23d834dc6288758ad0525682a36b6fb
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2019-08-26 13:38:34 -0300

    config graphql

---
+++ src/middlewares/apollo.ts
@@ -0,0 +1,23 @@
+import { ApolloServer, gql } from 'apollo-server-express'
+import { importSchema } from 'graphql-import'
+
+import directives from '../directives'
+import resolvers from '../resolvers'
+
+const schema = importSchema('./graphql/schema.graphql')
+const typeDefs = gql(schema)
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+  schemaDirectives: directives,
+  context: ({ req }) => ({
+    user: req.user,
+  })
+})
+
+export default {
+  set: app => {
+    server.applyMiddleware({ app })
+  },
+}
---
+++ src/resolvers/index.ts
@@ -0,0 +1,11 @@
+import { root as userRoot, queries as userQueries, mutations as userMutations } from './user'
+
+export default {
+  ...userRoot,
+  Query: {
+    ...userQueries,
+  },
+  Mutation: {
+    ...userMutations,
+  },
+}
