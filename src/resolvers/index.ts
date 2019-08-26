a6033180d3dce3b26ca3e5f2661a30a81dd5b2c4
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2019-08-26 16:12:45 -0300

    add store and item types
    Co-authored-by: Antunes Dantas <antunesdantas@outlook.com>

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -4,12 +4,25 @@ import {
   mutations as userMutations,
 } from './user'
 
+import { 
+  mutation as itemMutation,
+  query as itemQuery 
+} from './item'
+import { 
+  mutation as storeMutation,
+  query as storeQuery 
+} from './store'
+
 export default {
   ...userRoot,
   Query: {
     ...userQueries,
+    ...itemQuery,
+    ...storeQuery
   },
   Mutation: {
     ...userMutations,
+    ...itemMutation,
+    ...storeMutation
   },
 }
---
+++ src/resolvers/store.ts
@@ -0,0 +1,20 @@
+import Store, { StoreDocument } from '../models/Store'
+import Item from '../models/Item'
+
+export const query = {
+  store: (_: any, { id }: any) => Store.findById(id).exec().then(store => store)
+}
+
+export const mutation = {
+  createStore: (_: any, { name }: any) => Store.create({ name }).then(document => document),
+  addItem: (_: any, { item, storeId }: any) => Store.findById(storeId).exec().then(store => {
+    const { items } = store as StoreDocument;
+    return Item.findById(item).exec().then(data => {
+      const updatedStore = {
+        ...store,
+        items: [...items, data]
+      }
+      return Store.update(null, updatedStore).exec().then(() => updatedStore)
+    })
+  })
+}
