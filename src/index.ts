533e840fd7d7e83cb66ecb1cbc719db6bc73adb5
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2019-11-04 20:29:53 -0300

    Initialize firebase

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import morgan from 'morgan'
 import helmet from 'helmet'
+import firebase from 'firebase/app'
 
 import ioMiddleware from './middlewares/io'
 import authMiddleware from './middlewares/auth'
@@ -13,6 +14,19 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrsBtDr9TcePU2ikfoxxAk1tc4YX06--Y",
+  authDomain: "paguerapido-4061a.firebaseapp.com",
+  databaseURL: "https://paguerapido-4061a.firebaseio.com",
+  projectId: "paguerapido-4061a",
+  storageBucket: "paguerapido-4061a.appspot.com",
+  messagingSenderId: "550630617492",
+  appId: "1:550630617492:web:9b8ed0afd489e9a3c2df81",
+  measurementId: "G-YVBN785XCR"
+}
+
+firebase.initializeApp(firebaseConfig)
 
 app.use(helmet())
 app.use(morgan('dev'))
